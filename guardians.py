c8f8e17e2bf97bfee706d9a448767e62b2d95ca9
A Gamin <alicekiragamin@gmail.com>
2025-06-22 20:35:08 -0400

    Failed to get person_stats to work. May be broken?

--- guardians.py
+++ guardians.py
@@ -5,42 +5,40 @@ import statsapi
 from pprint import pprint
 import json
 import os
+from pprint import pprint
 from google.cloud import bigquery
 
-# os.chdir(os.path.expanduser("~\Documents\jupyter\MLB"))
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = os.path.expanduser("~/Documents/jupyter/MLB/credentials/mlb-analysis-463501-869e729270f3.json")
 
-# List of official MLB team IDs
-mlb_team_ids = [
-    108, 109, 110, 111, 112, 113, 114, 115,
-    116, 117, 118, 119, 120, 121, 133, 134,
-    135, 136, 137, 138, 139, 140, 141, 142,
-    143, 144, 145, 146, 147, 158
-]
 
 guardiansStats = []
 gamePk = 777429
 teamId = 114
 
-getRosterUrl = f"https://statsapi.mlb.com/api/v1/teams/{teamId}/roster"
+getRosterUrl = f"https://statsapi.mlb.com/api/v1/teams/{teamId}/roster?rosterType=25Man"
 roster = requests.get(getRosterUrl)
 roster = roster.json()
 roster_list = roster.get("roster",[])
 
-
+guardsStatsLife = []
+gamePk = 777411
 for player in roster_list:
-    print(keys(player))
     person = player['person']
-    # person = player.get('person',{})
     player_id = person['id']
+    #jersey = person['jerseyNumber']
     player_name = person['fullName']
     status = player['status']
+    getStatsUrl = f"https://statsapi.mlb.com/api/v1/people/{player_id}/stats?type=career"
+    playerStats = requests.get(getStatsUrl)
+    playerStats = playerStats.json()
+    print(f"Player ID: {player_id}, Name: {player_name}, Status: {status['description']}\n")
+    #print(f"Stats (lifetime): {playerStats['stats']['splits']}")
+    pprint(playerStats)
+    sys.exit()
+    guardsStatsLife.append(playerStats)
 
-    print(f"Player ID: {player_id}, Name: {player_name}, Status: {status}")
-
-
-client = bigquery.Client()
-print("Project:", client.project)
+# client = bigquery.Client()
+# print("Project:", client.project)
 
 sys.exit()
 
--- todays_games.py
+++ todays_games.py
@@ -2,7 +2,7 @@ import requests
 from datetime import date
 
 # # Get today's schedule
-today = date.today().isoformat()
+today = date.today().isoformat() 
 print(date.today())
 url = f"https://statsapi.mlb.com/api/v1/schedule?sportId=1&date={today}"
 
@@ -12,6 +12,6 @@ schedule = resp.json()
 # Grab all gamePks for today
 games = schedule['dates'][0]['games']
 for game in games:
-    print(f"{game['teams']['away']['team']['name']} ( {game['teams']['away']['team']['id']} ) at {game['teams']['home']['team']['name']} ({game['teams']['home']['team']['id']})")
+    print(f"{game['teams']['away']['team']['name']} ({game['teams']['away']['team']['id']}) at {game['teams']['home']['team']['name']} ({game['teams']['home']['team']['id']})")
     print(f"gamePk: {game['gamePk']}")
     
